cb87e9d0f9a8f4b71dcfff47e711bcb66e50e190
windows_on_wsl <otomijuf.004@gmail.com>
2024-02-13 00:22:04 +0900

    fix predict result

--- infarence.py
+++ infarence.py
@@ -1,7 +1,22 @@
 import argparse
+import numpy as np
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
+
+def id_to_label(id):
+    if id==0:
+        return "文学と芸術"
+    if id==1:
+        return "科学と技術"
+    if id==2:        
+        return "社会科学"
+    if id==3:
+        return "健康と医学"
+    if id==4:
+        return "ビジネスと経済"
+    if id==5:
+        return "レジャーとライフスタイル"
 
 def main():
     parser = argparse.ArgumentParser()
@@ -18,7 +33,9 @@ def main():
                     truncation=True,
                     return_tensors='pt')
     outputs = model(text.input_ids)
-    print(outputs)
+    y_preds = np.argmax(outputs.logits.to('cpu').detach().numpy().copy(), axis=1)
+    print('y_preds', y_preds)
+    print('label', id_to_label(y_preds))
 
 if __name__ == "__main__":
     main()
